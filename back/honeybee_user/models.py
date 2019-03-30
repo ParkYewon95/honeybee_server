32e61c222ff89acb26a2334070f424dbdaea45bf
parkyewon <angelwon53@gmail.com>
2019-03-30 22:49:44 +0900

    Auto stash before rebase of "origin/seongil"

--- back/honeybee_user/models.py
+++ back/honeybee_user/models.py
@@ -1,11 +1,7 @@
 import uuid
 from django.db import models
-<<<<<<< Updated upstream
 from django.contrib.auth.base_user import AbstractBaseUser,BaseUserManager
 from django.contrib.auth import models as auth_models
-=======
-from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
->>>>>>> Stashed changes
 
 class HoneyBeeManager(BaseUserManager):
     def create_user(self, id, name, email, password,**kwargs):
@@ -75,13 +71,7 @@ class HoneyBeeUser(AbstractBaseUser):
     objects = HoneyBeeManager()
 
     USERNAME_FIELD = 'id'
-<<<<<<< Updated upstream
     REQUIRED_FIELDS = ('email','name',)
-=======
-    REQUIRED_FIELDS = ('email',)
-    
-    objects = HoneyBeeManager()
->>>>>>> Stashed changes
 
     class Meta:
         db_table='honeybee_user'
