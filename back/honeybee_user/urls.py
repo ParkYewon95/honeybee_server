58a1f12b3b009b5154cb9c754aa8a566f6bbad99
parkyewon <angelwon53@gmail.com>
2019-04-04 20:25:58 +0900

    UserList, UserDetail API
    + url

---
+++ back/honeybee_user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from honeybee_user import views
+
+urlpatterns = [
+    path('honeybee_user/', views.USERLIST.as_view()),
+    path('honeybee_user/<int:pk>/', views.USERDETAIL.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
